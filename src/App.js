e1ce081596e5302f1cc6ccc68ea7fea6500c2578
zannatreya <zannatulbintabahar@yahoo.com>
2022-04-05 16:42:45 +0600

    finally done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Components/Blogs/Blogs';
 import Dashboard from './Components/Dashboard/Dashboard';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import NotFound from './Components/NotFound/NotFound';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/blogs" element={<Blogs />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
